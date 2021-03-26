c3ef2a94b06a584095f5ec687b01353ca4c22985
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-03-26 13:18:53 +0700

    V .0.1.0

---
+++ commands/information/botinfo.js
@@ -0,0 +1,4 @@
+module.exports = ({
+  name: "stats",
+  code: "$title[Bot Stats] \n $addField[Hosting; Uptime: $uptime] $addField[Physcal Driver;CPU: $cpu % \n Ram: $ram \n Ping: $ping ms] "
+})
---
+++ commands/information/info.js
@@ -0,0 +1,4 @@
+module.exports = ({
+  name: "info",
+  code: "$title[Bot Information]\n$description[Bot ini menggunakan code dari github RAYDENFLY dan di build menggunakan JavaScript]"
+})
