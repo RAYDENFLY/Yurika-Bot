00280add35c76c69e0e8c9940df4088f2e8aedfc
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-03-26 13:16:27 +0700

    help.js (v.0.01)
    help.js (v.0.01)

---
+++ commands/information/help.js
@@ -0,0 +1,4 @@
+module.exports = ({
+  name: "help",
+  code: "$title[Help Panel] \n $addField[Moderation;`ban` | `kick` | `clear`] \n $addField[Music;`play` | `pause` | `resume` | `stop` | `loop` | `queue` | `skip` | `skipto` | `nowplaying` | `volume`] \n $addField[Information;`stats` | `help`]"
+})
